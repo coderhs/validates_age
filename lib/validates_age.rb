d8aafc190310809c4b2efea22da105e23ef680c1
Harisankar P S <hsps@rubykitchen.org>
2013-03-08 18:37:55 +0530

    Added missing end of validator module

--- lib/validates_age.rb
+++ lib/validates_age.rb
@@ -4,7 +4,6 @@ module Rubykitchen
 	module ValidatesAge
 		module Validator
 			class AgeValidator < ActiveModel::EachValidator
-
 				def validate_age value
           if value > Time.now 
           	messages << "According to the Age provided the person is not born"
@@ -15,11 +14,11 @@ module Rubykitchen
           end
 				end
 			end
+		end
 
-			module ClassMethods
-				def validates_age_of(*attr_names)
-					validates_with ActiveRecord::Base::AgeValidator, _merge_attributes(attr_names)
-				end
+		module ClassMethods
+			def validates_age_of(*attr_names)
+				validates_with ActiveRecord::Base::AgeValidator, _merge_attributes(attr_names)
 			end
 		end
 	end
