2e817aff75de9f0c4a8b18035311af9e98ccbb62
Harisankar P S <hsps@rubykitchen.org>
2013-03-08 18:41:50 +0530

    Changed name of fucntion to validate_each, and added proper convention for it

--- lib/validates_age.rb
+++ lib/validates_age.rb
@@ -4,13 +4,13 @@ module Rubykitchen
 	module ValidatesAge
 		module Validator
 			class AgeValidator < ActiveModel::EachValidator
-				def validate_age value
-          if value > Time.now 
-          	messages << "According to the Age provided the person is not born"
-          elsif ((Time.now-value) < 13)
-          	messages << "The person is not old enough to join this website"
-          elsif ((Time.now-value) > 130)
-            messages << "The age entered is unrealestic"
+				def validate_each(record, attribute, value)
+          if value > Date.today  
+            record.errors[attribute] << "According to the Age provided the person is not born"
+          elsif ((Date.today -value) < 13)
+            record.errors[attribute] << "The person is not old enough to join this website"
+          elsif ((Date.today -value) > 130)
+            record.errors[attribute] << "The age entered is unrealestic"
           end
 				end
 			end
